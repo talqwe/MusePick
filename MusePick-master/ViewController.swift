4d6c541ef89475812f7b9cfae732a844bc688e8c
Tal Mishaan <talqwe@Tals-Mac.local>
2018-02-03 06:59:40 -0800

    Initial Commit

---
+++ MusePick-master/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusePick-master
+//
+//  Created by Tal Mishaan on 03/02/2018.
+//  Copyright © 2018 Tal Mishaan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
