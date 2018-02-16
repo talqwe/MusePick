bfb31a82848172b688743bca0c3d25b44d7ab625
Tal Mishaan <talqwe@Tals-Mac.local>
2018-02-16 12:47:06 -0800

    sqlite

---
+++ sqlite-bridge-file.h
@@ -0,0 +1,13 @@
+//
+//  sqlite-bridge-file.h
+//  MusePick-master
+//
+//  Created by Tal Mishaan on 16/02/2018.
+//  Copyright © 2018 Tal Mishaan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sqlite_bridge_file : NSObject
+
+@end
---
+++ sqlite-bridge-file.m
@@ -0,0 +1,13 @@
+//
+//  sqlite-bridge-file.m
+//  MusePick-master
+//
+//  Created by Tal Mishaan on 16/02/2018.
+//  Copyright © 2018 Tal Mishaan. All rights reserved.
+//
+
+#import "sqlite-bridge-file.h"
+
+@implementation sqlite_bridge_file
+
+@end
