7e2404869e76b2836f69bd376d5ea763ac01151c
Tal Mishaan <talqwe@Tals-Mac.local>
2018-02-27 11:10:15 -0800

    updated cell- like already works, not completely

--- Controllers/EventSongListController.swift
+++ Controllers/EventSongListController.swift
@@ -11,8 +11,9 @@ import UIKit
 class EventSongListController: UIViewController,UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
-    var event_id: String = ""
     var data = [Song]()
+    var like_dict = Dictionary<String,Like>()
+    var like_counter_dict = Dictionary<String,Int>()
 
     
     @IBAction func AddSong(_ sender: Any) {
@@ -28,33 +29,59 @@ class EventSongListController: UIViewController,UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: SongTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "song_cell") as! SongTableViewCell
         let song = data[indexPath.row]
-        cell.ArtistSongName.text = song.artist_name+" - "+song.song_name
+        cell.SongName.text = song.song_name
+        cell.ArtistName.text = song.artist_name
 
+        if(like_counter_dict[song.artist_name+":"+song.song_name] != nil){
+            cell.HeartImage.setTitle(String(like_counter_dict[song.artist_name+":"+song.song_name]!), for: UIControlState.normal)
+        }
+//        cell.like_counter_label.text = String(0)
+
+        Model.instance.getImage(urlStr: song.image! , callback: { (image) in
+            cell.SongImage.image = image
+        })
+        
+        var heart_picture_name = "emptyheart"
+        if(like_dict[Model.this_user.email+":"+song.artist_name+":"+song.song_name] != nil){
+            cell.is_like = true
+            heart_picture_name = "fullheart"
+        }
+        cell.HeartImage.setBackgroundImage(UIImage(named: heart_picture_name), for: UIControlState.normal)
+
+        
         return cell
     }
+
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destinationViewController = segue.destination as? AddSongController {
-            destinationViewController.event_id = event_id
-        }
-    }
-    
     override func viewDidLoad() {
-//        self.tableView.tableHeaderView = "Tal";
-        Model.getAllSongsAndObserve(eventID: event_id)
+        Model.getAllSongsAndObserve(eventID: Model.this_event.id_code)
+        Model.getAllLikesAndObserve(eventID: Model.this_event.id_code)
+        
+        _ = ModelNotification.LikeList.observe { (list) in
+            self.like_dict = list!
+
+        }
+        
+        for (string,like) in self.like_dict{
+            print("LikeLoop: "+string+": "+like.song_name)
+            if(!(like_counter_dict[like.artist_name+":"+like.song_name] == nil)){
+                like_counter_dict[like.artist_name+":"+like.song_name] = 1
+            }else{
+                like_counter_dict[like.artist_name+":"+like.song_name] = like_counter_dict[like.artist_name+":"+like.song_name]!+1
+            }
+        }
+        
         _ = ModelNotification.SongList.observe { (list) in
             if list != nil {
                 self.data = list!
                 self.tableView.reloadData()
+                
             }
-        }
-//        NotificationCenter.default.addObserver(self, selector:
-//            #selector(ViewController.songsListDidUpdate),
-//                                               name: NSNotification.Name(rawValue: notifySongListUpdate),object: nil)
-//        Model.instance.getAllSongsAndObserve(eventID: event_id)
+        }
         
         self.tableView.frame = CGRect(x:0,y:10, width:self.view.frame.width,height:self.view.frame.height - 40);
         self.tableView.delegate = self
@@ -64,8 +91,6 @@ class EventSongListController: UIViewController,UITableViewDelegate, UITableView
         
         self.view.addSubview(self.tableView)
         
-        
-//        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
--- Controllers/SongTableViewCell.swift
+++ Controllers/SongTableViewCell.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class SongTableViewCell: UITableViewCell {
-    @IBOutlet weak var ArtistSongName: UILabel!
+    var onButtonTap : (() -> Void)? = nil
+    var is_like: Bool = false
+    
+    @IBOutlet weak var SongName: UILabel!
+    @IBOutlet weak var ArtistName: UILabel!
+    @IBOutlet weak var like_counter_label: UILabel!
+    @IBOutlet weak var SongImage: UIImageView!
+    @IBOutlet weak var HeartImage: UIButton!
+    @IBAction func HeartAction(_ sender: UIButton) {
+        LikeHandle(c: self)
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    }
+    
+    func LikeHandle(c: SongTableViewCell) {
+        if(c.is_like){
+            c.is_like = false
+            c.like_counter_label.text! = String(Int(c.like_counter_label.text!)!+1)
+            c.HeartImage.setBackgroundImage(UIImage(named: "emptyheart"), for: UIControlState.normal)
+        }else{
+            c.is_like = true
+            c.like_counter_label.text! = String(Int(c.like_counter_label.text!)!-1)
+            c.HeartImage.setBackgroundImage(UIImage(named: "fullheart"), for: UIControlState.normal)
+            let s = Song(event_id: Model.this_event.id_code, song_name: c.SongName.text!, artist_name: c.ArtistName.text!, image: "")
+            Model.instance.addLike(s: s, email: Model.this_user.email)
+        }
     }
 
 }
